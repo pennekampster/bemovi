3fcd7c805dfbcbae53acbbcf0aa03d57f2f8a633
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2013-01-14 10:48:05 +0100

    Updated function to run the ParticleTracker macro from within R; tracking on centroids

--- automation/batch_process_videos.R
+++ automation/batch_process_videos.R
@@ -1,9 +1,6 @@
 # code to batch process videos by ImageJ and ParticleTracker plugin
-# better provide function specifying the path where videos are
 
-# specify directory where videos are
-video.dir = "C:/Users/Frank/Desktop/test/"
-
+video_to_trajectory <- function(video.dir) {
 # copy master copy of ImageJ macro there for treatment
 text <- readLines("C:/Users/Frank/Documents/PhD/Programming/franco/automation/ImageJ macros/Video_to_trajectory.ijm")
 
@@ -12,14 +9,82 @@ text[3] <- sub(text, "dir_input = ", paste("dir_input = ","'", video.dir,"';", s
 text[4] <- sub(text, "dir_output = ", paste("dir_output = ","'", video.dir,"';", sep = ""))
 
 # re-create ImageJ macro for batch processing of video files with ParticleTracker
-writeLines(text,con=paste("C:/Program Files/Fiji.app/Video_to_trajectory_tmp.ijm",sep=""),sep="\n")
+writeLines(text,con=paste("C:/Program Files/Fiji.app/macros/Video_to_trajectory_tmp.ijm",sep=""),sep="\n")
 
-# run to process video files by calling ImageJ 
+# run to process video files by calling ImageJ / needs fixing for Mac
 if(.Platform$OS.type == "unix"){
 cmd <- c("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ")}
 if(.Platform$OS.type == "windows"){
-cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe -macro Video_to_trajectory_tmp.ijm"')}
+cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Video_to_trajectory_tmp.ijm')}
 system(cmd)
 
 #delete temporary file after execution
-file.remove("C:/Program Files/FIJI.app/Video_to_trajectory_tmp.ijm")
+file.remove("C:/Program Files/Fiji.app/macros/Video_to_trajectory_tmp.ijm")
+}
+
+## This function gets the output files produced by the Imagej ParticleTracker
+LoadIJ_Traj_Outs <- function(video.dir) {
+  
+# function to create a unique trajectory ID with each processed result file of the ParticleTracker
+unique_traj_ID <- function(dataset){
+    
+lag <- dataset[2:nrow(dataset),1]
+lag <- append(lag,1)
+dataset <- cbind(dataset,lag)
+    
+# assign a counter creating a unique trajectory
+trajectory <- 1
+for (i in 1:nrow(dataset)){
+    if (dataset[i,12]>dataset[i,1]){
+        dataset[i,"trajectory"] <- trajectory}
+      
+    if (dataset[i,12]<=dataset[i,1]){
+        dataset[i,"trajectory"] <- trajectory
+        trajectory <- trajectory+1}
+    } 
+dataset$lag <- NULL
+# function to convert object name into character string
+myfun <- function(x) deparse(substitute(x)) 
+assign(paste(myfun(dataset)),dataset,envir = .GlobalEnv)
+}
+  
+## the macro file names
+all.files <- dir(path=video.dir)
+ijout.files <- all.files[grep("Traj_", all.files)]
+
+for (i in 1:length(ijout.files)){
+text <- readLines(paste(video.dir, ijout.files[i], sep="/"))
+  
+string <- "(^[[:digit:]]+)"
+#find lines which match the string
+txtsubset <-  grep(string, text) 
+#subset the original file by only retaining lines that matched the string
+out <- text[txtsubset]
+  
+if (i == 1){
+dd <- as.data.frame(read.table(textConnection(out)))
+dd$file <- rep(ijout.files[1], length(dd$V1))
+dd <- unique_traj_ID(dd)
+}
+  
+if (i > 1){
+for(j in 2:length(ijout.files)) {
+    
+dd.t <- as.data.frame(read.table(textConnection(out)))
+dd.t$file <- rep(ijout.files[i], length(dd.t$V1))
+dd.t <- unique_traj_ID(dd.t)
+dd <- rbind(dd, dd.t)}
+}}
+
+# invert Y-axis to account for origin in upper-left corner
+dd$V2 <- -dd$V2
+
+#rename drop unused information and rename variables
+dd <- dd[c(-4,-5,-6,-7,-8,-9,-10)]
+names(dd) <- c("frame","X","Y","file","trajectory")
+
+assign("trajectory.data",dd,envir = .GlobalEnv)
+write.table(trajectory.data, file = paste(video.dir,"trajectory.data.txt", sep = "/"), sep = "\t")
+}
+
+
