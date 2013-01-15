188e734da710549db7ce700f5cda482ee69f75d0
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2013-01-15 10:09:59 +0100

    Function to automatically create overlay videos is now fully functional

--- merge morphology and trajectory/create overlay.r
+++ merge morphology and trajectory/create overlay.r
@@ -33,20 +33,45 @@ dev.off()
 # function to plot trajectories for overlay (must be merged with original video by ImageJ macro)
 # creates a folder containing one jpeg plot containing all the positions till the respective frame
 # for the moment colour not assigned by species identity but that's easy to add 
-create_overlay_plots <- function(path,duration){ #should be taken automatically from the file containing the position and frame data
+# provide path of " 2 - trajectory data"
+create_overlay_plots <- function(path){ 
 trajectory.data <- as.data.frame(read.table(paste(path,"trajectory.data.txt", sep = ""), header = TRUE, sep = "\t"))
 file_names <- unique(trajectory.data$file)  
+# change path for output
+dir.create(sub("2 - trajectory data/","3 - overlay plots/",path))
 for (i in 1:length(file_names)){
-dir.create(paste(path,substr(file_names[i],6,11),sep="/"))
-duration <- duration
+dir.create(paste(sub("2 - trajectory data/","3 - overlay plots/",path),substr(file_names[i],6,11),sep="/"))
 trajectory.data_tmp <- subset(trajectory.data,file == file_names[i])
 j<- 0
-while(j < duration){
-jpeg(paste(path,substr(file_names[i],6,11),"/frame_",j,".jpg",sep=""), width = 2048, height = 2048, quality = 100)
+while(j < max(trajectory.data$frame)+1){
+jpeg(paste(sub("2 - trajectory data/","3 - overlay plots/",path),substr(file_names[i],6,11),"/frame_",j,".jpg",sep=""), width = 735, height = 690, quality = 100)
 par(mar = rep(0, 4), xaxs=c("i"), yaxs=c("i"))
 print <- subset(trajectory.data_tmp,trajectory.data_tmp$frame <= j, select=c("X","Y","trajectory"))
-plot(print$Y, print$X+2048, xlim=c(0,2048), ylim=c(0,2048), col="#FFFF00", pch=15, cex=1, asp=1)
+plot(print$Y, print$X+690, xlim=c(0,735), ylim=c(0,690), col="#FFFF00", pch=15, cex=1, asp=1)
 dev.off()
-j <- j+1}}}
+j <- j+1}}
 
-create_overlay_plots("C:/Users/Frank/Documents/PhD/Programming/franco/videos/",101)
+# copy master copy of ImageJ macro there for treatment
+text <- readLines("C:/Users/Frank/Documents/PhD/Programming/franco/automation/ImageJ macros/Video_overlay.ijm")
+
+# use regular expression to insert input and output directory
+text[3] <- sub(text, "avi_input = ", paste("avi_input = ","'", sub("2 - trajectory data/","1 - raw/",path),"';", sep = ""))
+text[4] <- sub(text, "overlay_input = ", paste("overlay_input = ","'", sub("2 - trajectory data/","3 - overlay plots/",path),"';", sep = ""))
+text[5] <- sub(text, "overlay_output = ", paste("overlay_output = ","'", sub("2 - trajectory data/","4 - overlays/",path),"';", sep = ""))
+
+# re-create ImageJ macro for batch processing of video files with ParticleTracker
+writeLines(text,con=paste("C:/Program Files/Fiji.app/macros/Video_overlay_tmp.ijm",sep=""),sep="\n")
+
+# create directory to store overlays
+dir.create(sub("2 - trajectory data/","4 - overlays/",path))
+
+#call IJ macro to merge original video with the trajectory data
+if(.Platform$OS.type == "unix"){
+  cmd <- c("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ")}
+if(.Platform$OS.type == "windows"){
+  cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Video_overlay_tmp.ijm')}
+system(cmd)
+
+}
+
+create_overlay_plots("C:/Users/Frank/Documents/PhD/Programming/franco/data/2 - trajectory data/")
