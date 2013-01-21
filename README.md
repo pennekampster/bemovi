bedf4d375316b70284ae310cb15337c1372378ea
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2013-01-21 20:59:19 +0100

    Updated code to use trajectory database; calculation of movement metrics and subsetting raw data to get rid of spurious trajectories

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 franco
-======
+=============================================================================
 
 R and imageJ code for video-based counting and identification of aquatic
 microbes, with methods to visualising and check the results.
 
 There are several folders:
 
-==== Data ========
+Data
+-----------------------------------------------------------------------------
 A folder containing several subfolders to store the sample description, the 
 raw data, and folders which are created by the functions to store their 
 respective results. The videos to be analyzed are stored in the subfolder 
 "data/1 - raw/" which needs to be set up by the user. The folders which are
 used by the functions are automatically created and the files transferred.
 
-=== Automation ===
+Automation
+-----------------------------------------------------------------------------
 We want to be able to supply a collection of videos of aquatic
 microbes, and get back the number of individuals of each species, and
 also some of their traits (size, speed, etc.).
@@ -29,20 +31,24 @@ Both, extraction of morphology and particle tracking is automated, but the
 present way of tracking maybe replaced.
 To run the analysis, use the file eg.worker.R 
 
-=== Merge morphology and trajectory ===
+Merge morphology and trajectory
+----------------------------------------------------------------------------
 Example of merging morphological data acquired with the particle
 analyser of ImageJ, and trajectory data acquired with the MOSAIC
 particle tracker. (Obsolete if ParticleTracker can be run on coordinates from
 ParticleAnalyzer; merging between morphology and tracking based on current 
 approach not perfectly matching.)
 
-=== Movement analysis ==========
+Movement analysis
+----------------------------------------------------------------------------
 A script which uses the coordinate data extracted from the videos via the
 ParticleTracker and aggregates the data into movement metrics such as gross
 and net displacement, average speed and linearity (net-to-gross-displacement.
 Moreover, statistics of turning angle distribution and autocorrelation in 
 turning angles distribution are calculated via the adehabitatLT package.
 
-=== Morphological analysis ===
+Morphological analysis
+-----------------------------------------------------------------------------
 Example of what one can do with morphology, using an ANN to
-discriminate species.
+discriminate species. The next step is to include information of movement behaviour
+into the classification process.
--- movement analysis/Movement analysis for species recognition in R.R
+++ movement analysis/Movement analysis for species recognition in R.R
@@ -8,108 +8,46 @@ library(adehabitatLT)
 library(grid)
 library(plyr)
 
-text <- readLines("C:\\Video analysis for species recognition\\selected trajects\\Traj_Data45.avi.txt")
-string <- "(^[[:digit:]]+)"
-
-#find lines which match the string
-txtsubset <-  grep(string, text) 
-
-#subset the original file by only retaining lines that matched the string
-out <- text[txtsubset]
-
-#save resulting text file after cleaning
-writeLines(out,con="c:\\users\\Frank\\documents\\file.txt",sep="\n")
-
-csv <- read.csv("c:\\users\\Frank\\documents\\file.txt", header=FALSE, sep=" ")
-
-lag <- csv[2:nrow(csv),1]
-lag <- append(lag,1)
-final <- cbind(csv,lag)
-
-# assign a counter creating a unique trajectory
-trajectory <- 1
-for (i in 1:nrow(final)){
-  if (final[i,11]>final[i,1]){
-      final[i,"trajectory"] <- trajectory}
-  
-  if (final[i,11]<=final[i,1]){
-      final[i,"trajectory"] <- trajectory
-      trajectory <- trajectory+1}
-} 
-
-# invert Y-axis to account for origin in upper-left corner
-final$V2 <- -final$V2
-
-#rename drop unused information and rename variables
-final$lag <- NULL
-final <- final[c(-4,-5,-6,-7,-8,-9,-10)]
-final$trajectory <- final$trajectory
-names(final) <- c("frame","X","Y", "trajectory")
-
-
+# load trajectory data
+trajectory.data <- read.table("C:/Users/Frank/Documents/PhD/Programming/franco/data/2 - trajectory data/trajectory.data.txt", header=TRUE, sep="\t")
 
 #calculate summary stats (count of frames) and merge with original data for trajectory selection
-count <- tapply(final$frame, final$trajectory, function(x) length(unique(x)))
-count <- as.data.frame(cbind(count,unique(final$trajectory)))
-names(count) <- c('frame_total','trajectory')
-final_summary <- merge(count,final,by=c("trajectory"))
-
-minimum <- ddply(final_summary, .(trajectory), .fun = function(a){a[which.min(a$frame), ]})
-maximum <- ddply(final_summary, .(trajectory), .fun = function(a){a[which.max(a$frame), ]})
-
-minimum <- minimum[c(-2)]
-names(minimum) <- c("trajectory","frame_start","X_start","Y_start")
-
-maximum <- maximum[c(-2)]
-names(maximum) <- c("trajectory","frame_end","X_end","Y_end")
-
-net_disp <- merge(minimum,maximum,by=c("trajectory"))
-
+start_frame <- ddply(trajectory.data, .(trajectory,file), .fun = function(a){a[which.min(a$frame), ]})
+end_frame <- ddply(trajectory.data, .(trajectory,file), .fun = function(a){a[which.max(a$frame), ]})
+names(start_frame) <- c("start_frame","X_start","Y_start","file","trajectory")
+names(end_frame) <- c("end_frame","X_end","Y_end","file","trajectory")
+net_disp <- merge(start_frame,end_frame,by=c("trajectory","file"))
 net_disp$net_disp <- round(sqrt((net_disp$X_end - net_disp$X_start)^2 + (net_disp$Y_end - net_disp$Y_start)^2))
 #net speed in pixels/second
-net_disp$duration <- (net_disp$frame_end - net_disp$frame_start)/27
+net_disp$duration <- (net_disp$end_frame - net_disp$start_frame)/27
 net_disp$net_speed <- net_disp$net_disp/net_disp$duration
-final_summary <- merge(final,net_disp,by=c("trajectory"))
-final_summary <- subset(final_summary, final_summary$net_speed > 20 & final_summary$net_disp > 10)
+net_disp_summary <- net_disp[c(1,2,9,10,11)]
 
-
-#plot trajectories
-ggplot(final_summary, aes(x=final_summary$X, y=final_summary$Y, color=as.factor(final_summary$trajectory))) +
-geom_point(shape=19, size = 0.1, alpha=0.5) +  
-theme_bw() +
-coord_fixed(xlim=c(-2048,0),ylim=c(0,2048))+
-theme(panel.grid.minor=element_blank(), 
-      panel.grid.major=element_blank(),
-      axis.text.x  = element_blank(),
-      axis.title.x = element_blank(),
-      axis.text.y  = element_blank(), 
-      axis.title.y = element_blank(),
-      axis.ticks = element_blank(),
-      plot.background=element_blank(),
-      panel.border = element_blank(),
-      panel.margin = unit(0,"cm"),
-      plot.margin = rep(unit(0,"cm"),4),
-      axis.ticks.length = unit(0.001,"cm"),
-      axis.ticks.margin = unit(0.001,"cm"),
-      legend.position="none") +
-scale_x_continuous(expand=c(0,0)) + 
-scale_y_continuous(expand=c(0,0))
+#merge summary data with original trajectories
+trajectory.data.summary <- merge(trajectory.data,net_disp_summary,by=c("trajectory","file"))
 
 #subset data for movement analysis
-final <- subset(final_summary, (trajectory < 50  ))
+trajectory.data.summary <- subset(trajectory.data.summary, file == "Traj_Data34.avi.txt")
+trajectory.data.summary <- subset(trajectory.data.summary, (net_speed > 50 &  net_speed < 300) & net_disp > 50)
 
-final$sec <- final$frame  
-final$da <- as.POSIXct(final$sec, origin = "1900-01-01", format = "%OS")
+attach(trajectory.data.summary_subset)
+plot(Y, X+as.numeric(height), xlim=c(0,as.numeric(width)), ylim=c(0,as.numeric(height)), pch=15, cex=0.5, asp=1, col=net_speed)
+detach(trajectory.data.summary_subset)
+
+
+trajectory.data.summary$sec <- trajectory.data.summary$frame  
+trajectory.data.summary$datetime <- as.POSIXct(trajectory.data.summary$sec, origin = "1900-01-01", format = "%OS")
+trajectory.data.summary$sec <- NULL
 options(digits.secs=2)
 
 # convert raw data in ltraj class (typeII=FALSE meaning that no time was recorded)
-mvt_data <- as.ltraj(xy = final[,c("X","Y")], date = final$da, typeII=TRUE, id = final$trajectory)
+mvt_data <- as.ltraj(xy = trajectory.data.summary[,c("X","Y")], date = trajectory.data.summary$datetime, typeII=TRUE, id = trajectory.data.summary$trajectory)
 
 # add positions where locations should have been measured, but have not due to detection problems ..
-mvt_data <- setNA(mvt_data, date.ref = final$da, dt=1, units=c("sec"))
+mvt_data <- setNA(mvt_data, date.ref = trajectory.data.summary$datetime, dt=1, units=c("sec"))
 
 #check whether adding positions rendered trajectory regular
-#is.regular(mvt_data)
+is.regular(mvt_data)
 
 #to interpolate the positions which are missing, put the time interval (dt) to 1 
 mvt_data <- redisltraj(na.omit(mvt_data), 1, type="time")
@@ -155,5 +93,5 @@ mvt_data_df <- ld(mvt_data)
 plotltr(mvt_data, "R2n")
 
 # access ltraj object instances i.e. single trajectories
-plot(mvt_data[[3]]$x,mvt_data[[3]]$y)
+plot(mvt_data[[4]]$x,mvt_data[[4]]$y)
 
