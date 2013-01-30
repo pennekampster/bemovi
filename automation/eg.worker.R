74042ba3d68eb78ecd0e993abbdcdd7050bde6b0
owen-petchey <owen.petchey@ieu.uzh.ch>
2013-01-30 17:54:01 +0100

    Change the name of two the two particle analyser functions

--- automation/eg.worker.R
+++ automation/eg.worker.R
@@ -9,6 +9,9 @@ rm(list=ls())
 
 ## specify difference.lag for both Particle Analyzer as well as Tracker
 difference.lag <- 25
+## specify threshold values
+thresholds = c(0,999)
+
 
 
 ## Owen's paths
@@ -56,13 +59,13 @@ file.sample.info <- read.table(paste(sample.dir, sample.description.file, sep=""
 video.dir <- paste(to.data, raw.video.folder, sep="")
 IJ_output.dir <- paste(to.data, particle.analyzer.folder, sep="")
 #specify directory and difference.lag
-## MakeIJMacros(video.dir,difference.lag)
-## LoadIJOuts(IJ_output.dir)
+video_to_morphology(video.dir,difference.lag,thresholds)
+LoadIJ_morph_outs(IJ_output.dir)
 
 
 # run ParticleTracker, merge results and produce overlays
 video.dir <- paste(to.data, raw.video.folder, sep="")
-##video_to_trajectory(video.dir,difference.lag)
+video_to_trajectory(video.dir,difference.lag)
 
 
 # merge trajectory data into database
