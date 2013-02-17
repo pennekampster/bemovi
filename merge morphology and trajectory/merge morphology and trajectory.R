aa301f02e3355ea2a31d3b9d07c6c6c587ec4c52
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2013-02-17 17:30:57 +0100

    I will temporarily work on the merged morphology and trajectory data; the merge between both is not perfect, but good enough to explore how well species can be discriminated by morphology vs. movement metrics

--- merge morphology and trajectory/merge morphology and trajectory.R
+++ merge morphology and trajectory/merge morphology and trajectory.R
@@ -30,24 +30,49 @@ if(.Platform$OS.type == "unix"){
 trajectory.data <- read.table(paste0(to.data,trajectory.data.folder,"trajectory.data.txt"), row.names=1)
 trajectory.data$file <- gsub("Traj_" ,"",trajectory.data$file)
 trajectory.data$file <- gsub(".avi.txt" ,"",trajectory.data$file)
-trajectory.data$X <- round_any(-trajectory.data$X, 10)
-trajectory.data$Y <- round_any(trajectory.data$Y, 10)
+trajectory.data$X <- round_any(-trajectory.data$X, 5)
+trajectory.data$Y <- round_any(trajectory.data$Y, 5)
+# trajectory frame starts with 0, therefore add one to adjust to morphology data
+trajectory.data$frame <- trajectory.data$frame+1
 
 #load morphological.data
 morphology.data <- read.table(paste0(to.data,particle.analyzer.folder,"morphology.data.txt"), row.names=1)
 morphology.data$frame <- morphology.data$Slice
 morphology.data$Slice <- NULL
 morphology.data$file <- gsub(".ijout.txt" ,"",morphology.data$file)
-morphology.data$X <- round_any(morphology.data$X, 10)
-morphology.data$Y <- round_any(morphology.data$Y, 10)
+morphology.data$X <- round_any(morphology.data$X, 5)
+morphology.data$Y <- round_any(morphology.data$Y, 5)
 
-
-subset_m <- subset(morphology.data, file == "Data34")
-subset_t <- subset(trajectory.data, file == "Data34")
-
-plot(subset_m$X,subset_m$Y,asp=1,col="red")
-par(new=T)
-plot(subset_t$Y,subset_t$X,col="blue",asp=1)
+# subsample to do visual control
+#subset_m <- subset(morphology.data, file == "Data34")
+#subset_t <- subset(trajectory.data, file == "Data34")
+# check why X and Y differ between trajectories and morphology
+#plot(subset_m$X,subset_m$Y,pch=16,asp=1,col="red")
+#par(new=T)
+#plot(subset_t$Y,subset_t$X,col="blue",pch=1,asp=1, type="p")
 
 # merge morphological and trajectory data based on frame, file and X and Y
-merge <- sqldf("select * from subset_t t, subset_m m where t.X=m.X and t.Y=m.Y and t.frame=m.frame")
+# X and Y are changed between data sources, still have to find where confounding takes place
+merge <- sqldf("select *  
+                from 'trajectory.data' t
+                left join 
+                'morphology.data' m
+                on t.file=m.file AND t.X=m.Y AND t.Y=m.X AND t.frame=m.frame;
+               ")
+
+morphology_summary <- sqldf("select 
+                            file,
+                            trajectory,
+                            avg(area) as area,
+                            avg(mean) as grey,
+                            avg(perimeter) as perimeter,
+                            avg(major) as major,
+                            avg(minor) as minor,
+                            avg(AR) as AR,
+                            avg(circ_) as circularity
+                            from merge
+                            group by file, trajectory
+                            ")
+
+# export aggregated data on morphology
+write.table(morphology_summary, file = paste("C:/Users/Frank/Documents/PhD/Programming/franco/merge morphology and trajectory","morphology.data.summary.txt", sep = "/"), sep = "\t")
