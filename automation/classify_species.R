70b3aef4a5078c0f6ab55ae77dc7d705f64e4db7
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2013-03-31 12:31:25 +0200

    Further improvements on the function to visualize the predictions: objects that are not identified are also tracked

--- automation/classify_species.R
+++ automation/classify_species.R
@@ -1,32 +1,41 @@
-library(ggplot2)
-library(nnet)
+library(plyr)
 library(sqldf)
 library(randomForest)
-library(plyr)
 
 # code to do classification based on both morphology and movement data
 # will integrate all of OWen's previous ideas and re-use available codes
 
 ## Owen's paths
-to.data.owen <- "/Users/owenpetchey/Desktop/franco.test.vids/data/6 - Classification/"
+to.code.owen <- "/Users/owenpetchey/work/git/franco/automation/"
+to.data.owen <- "/Users/owenpetchey/Desktop/hard.test/"
 
 ## Frank's paths
-to.data.frank <- "C:/Users/Frank/Documents/PhD/Programming/franco/data/6 - Classification/"
+to.code.frank <- "C:/Users/Frank/Documents/PhD/Programming/franco/automation/"
+to.data.frank <- "C:/Users/Frank/Documents/PhD/Programming/franco/data/"
 
 ## what OS are we on?
 OS <- .Platform$OS.type
 ## if on windows, use Frank's paths
 if(.Platform$OS.type == "windows"){
+  to.code <- to.code.frank
   to.data <- to.data.frank}
 ## otherwise use Owen's
 if(.Platform$OS.type == "unix"){
+  to.code <- to.code.owen
   to.data <- to.data.owen}
 
+# specify folder for saving the predictions (jpeg plots) and the video overlays
+raw.video.folder <- "1 - raw/"
+trajectory.data.folder <- "2 - trajectory data/"
+class.data <- "6 - Classification/"
+prediction.folder <- "7 - Prediction/"
+prediction.folder2 <- "8 - Prediction overlay/"
+
 # import morphology data
-morphology.data <- read.table(paste0(to.data,"morphology.data.summary.txt"), sep = "\t")
+morphology.data <- read.table(paste0(to.data,class.data,"morphology.data.summary.txt"), sep = "\t")
 
 # import trajectory data
-trajectory.data <- read.table(paste0(to.data,"trajectory.data.summary.txt"), sep = "\t")
+trajectory.data <- read.table(paste0(to.data,class.data,"trajectory.data.summary.txt"), sep = "\t")
 
 #merge morphology data with meaningful trajectory data (meaningful = show properties used for filter trajectories)
 all_data <- sqldf("select *
@@ -37,64 +46,32 @@ all_data <- sqldf("select *
 # labelling files according to species
 all_data$species <- factor(all_data$file,
                     levels = c("Data34","Data38","Data45","Data49","Data52","Data57"),
-                    labels = c("Colpidium", "Paramecium", "Colpidum&Paramecium","Loxocephalus","Loxocephalus&Colpidum","Colpidum&Paramecium&Loxocephalus")) 
+                    labels = c("Colpidium", "Paramecium", "Colpidium&Paramecium","Loxocephalus","Loxocephalus&Colpidium","Colpidium&Paramecium&Loxocephalus")) 
 
 # only take monocultures
 species_subsets <- sqldf("select *
                          from all_data
                          where species in ('Colpidium', 'Paramecium', 'Loxocephalus')")
 
-# temporary plotting of aggregate variables to check which have highest discrimination power
-# 1. morphology
-ggplot(species_subsets, aes(x=species_subsets$area, color=species_subsets$species)) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$grey, color=species_subsets$species)) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$major, color=species_subsets$species)) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$perimeter, color=species_subsets$species)) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$circularity, color=species_subsets$species)) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$AR, color=species_subsets$species)) + geom_density()
 
-# 2. movement characteristics
-ggplot(species_subsets, aes(x=species_subsets$net_speed, color=as.factor(species_subsets$species))) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$NGDR, color=as.factor(species_subsets$species))) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$sd_turning, color=as.factor(species_subsets$species))) + geom_density()
-ggplot(species_subsets, aes(x=species_subsets$period, color=as.factor(species_subsets$species))) + geom_density()
-
-# use a PCA to visualize whether species can be separated by morphology & movement
-fit_move <- princomp(species_subsets[, c(4,5,6,7,8,9)], cor=TRUE)
-fit_morph <- princomp(species_subsets[, c(14,15,16,17,18,19,20)], cor=TRUE)
-PC1_morph <- fit_morph$scores[,1]
-PC2_morph <- fit_morph$scores[,2]
-PC1_move <- fit_move$scores[,1]
-PC2_move <- fit_move$scores[,2]
-plot_PCA_morph <- cbind(PC1_morph,PC2_morph,species_subsets[c(2,3,21)])
-plot_PCA_move <- cbind(PC1_move,PC2_move,species_subsets[c(2,3,21)])
-ggplot(plot_PCA_morph, aes(x=PC1_morph, y=PC2_morph, color=plot_PCA_morph$species)) + geom_point()
-ggplot(plot_PCA_move, aes(x=PC1_move, y=PC2_move, color=plot_PCA_move$species)) + geom_point()
-
-# ANN (based on morphological data)
 # take monocultures of three species and classify by morphology (excluding the grey value for the moment)
 three_moncultures <- sqldf("select *
                        from all_data
                        where species in ('Colpidium','Paramecium', 'Loxocephalus')")
 
-train <- cbind(three_moncultures[, c(14,16,17,18,19,20)])
-target <- class.ind(three_moncultures[,"species"])
-target <- target[, c(1,2,4)]
-samp <- sample(1:length(train[,1]), length(train[,1])/2)
-no.samp <- c(1:length(train[,1]))[-samp]
-nn <- nnet(train[samp,], target[samp,], size = 4, rang = 0.1, decay = 5e-4, maxit = 200)
+# define classification table function
 test.cl <- function(true, pred) {
   true <- max.col(true)
   cres <- max.col(pred)
   table(true, cres)
 }
-test.cl(target[no.samp,], predict(nn, train[no.samp,]))
 
-# try randomForest classification
+# test randomForest classification for species recognition
 # use factor function to only classify species comprised in the dataset
 train_rf <- cbind(three_moncultures[, c(4,5,6,7,8,9,10,11,14,15,16,17,18,19,20,21)])
 samp <- sample(1:length(train_rf[,1]), length(train_rf[,1])/2)
 no.samp <- c(1:length(train_rf[,1]))[-samp]
+# train randomForest
 rf_fit <- randomForest(factor(species) ~ net_speed + period + NGDR + sd_turning + grey + area + perimeter + major + minor + AR , data=train_rf[samp,], importance=TRUE, proximity=TRUE)
 print(rf_fit)
 plot(rf_fit)
@@ -102,64 +79,14 @@ varImpPlot(rf_fit)
 MDSplot(rf_fit, train_rf[samp,]$species)
 table(factor(train_rf[no.samp,]$species), predict(rf_fit, train_rf[no.samp,]))
 
-
-# train NN on monocultures and test on mixed cultures (classification without grey value!!!!!)
-# 1. Colpidium vs. Paramecium
-two_monocult <- sqldf("select *
-                         from all_data
-                         where species in ('Colpidium', 'Colpidium')")
-
-mixed_cult <- sqldf("select *
-                         from all_data
-                         where species in ('Colpidum&Paramecium')")
-
-train <- cbind(two_monocult[, c(14,16,17,18,19,20)])
-target <- class.ind(two_monocult[,"species"])
-target <- target[, c(1,2)]
-nn <- nnet(train, target, size = 4, rang = 0.1, decay = 5e-4, maxit = 200)
-classified <- predict(nn, mixed_cult[, c(14,16,17,18,19,20)])
-predict_spec <- as.data.frame(max.col(classified))
-names(predict_spec) <- c("predict_spec")
-predict <- cbind(mixed_cult[,1:3],predict_spec)
-predict$predict_spec <- as.factor(ifelse(predict$predict_spec == 1,"Colpidum","Paramecium"))
-
-# 2. Colpidium vs. Loxocephalus
-two_monocult <- sqldf("select *
-                         from all_data
-                         where species in ('Colpidium', 'Loxocephalus')")
-
-mixed_cult <- sqldf("select *
-                    from all_data
-                    where species in ('Loxocephalus&Colpidum')")
-
-train <- cbind(two_monocult[, c(14,16,17,18,19,20)])
-target <- class.ind(two_monocult[,"species"])
-target <- target[, c(1,4)]
-nn <- nnet(train, target, size = 4, rang = 0.1, decay = 5e-4, maxit = 200)
-classified <- predict(nn, mixed_cult[, c(14,16,17,18,19,20)])
-predict_spec <- as.data.frame(max.col(classified))
-names(predict_spec) <- c("predict_spec")
-predict <- cbind(mixed_cult[,1:3],predict_spec)
-predict$predict_spec <- as.factor(ifelse(predict$predict_spec == 1,"Colpidum","Loxocephalus"))
-
-# 3. Colpidium vs. Paramecium vs. Loxocephalus
+# use monoculture to predic species identity in communities
 three_monocult <- sqldf("select *
                          from all_data
                          where species in ('Colpidium','Paramecium','Loxocephalus')")
 
 mixed_cult <- sqldf("select *
                     from all_data
-                    where species in ('Colpidum&Paramecium&Loxocephalus')")
-
-train <- cbind(three_monocult[, c(14,16,17,18,19,20)])
-target <- class.ind(three_monocult[,"species"])
-target <- target[, c(1,2,4)]
-nn <- nnet(train, target, size = 4, rang = 0.1, decay = 5e-4, maxit = 200)
-classified <- round(predict(nn, mixed_cult[, c(14,16,17,18,19,20)]),2)
-predict_spec <- as.data.frame(max.col(classified))
-names(predict_spec) <- c("predict_spec")
-predict <- cbind(mixed_cult[,1:3],predict_spec)
-predict$predict_spec <- as.factor(ifelse(predict$predict_spec == 1,'Colpidum',ifelse(predict$predict_spec == 2,'Paramecium','Loxocephalus')))
+                    where species in ('Colpidium&Paramecium', 'Loxocephalus&Colpidium', 'Colpidium&Paramecium&Loxocephalus')")
 
 # classification by randomForest
 train_rf <- cbind(three_monocult[, c(4,5,6,7,8,9,10,11,14,15,16,17,18,19,20,21)])
@@ -171,31 +98,30 @@ predict <- cbind(mixed_cult,predict_spec)
 trajectory_raw <- read.table(paste0("C:/Users/Frank/Documents/PhD/Programming/franco/data/2 - trajectory data/trajectory.data.txt"), header=TRUE, sep="\t")
 trajectory_raw$id <- paste(trajectory_raw$file,trajectory_raw$trajectory,sep="-")
 
-predict_visual <- sqldf("select p.id, p.predict_spec, t.*
+predict_visual <- sqldf("select t.*, p.predict_spec 
                         from trajectory_raw t
                         left join predict p 
-                        on p.id=t.id 
-                        where p.file = 'Data57'")
+                        on p.id=t.id
+                        where t.file in ('Data45','Data52','Data57')")
 
 predict_visual$predict_spec <- factor(predict_visual$predict_spec)
 
-
-prediction.folder <- "7 - Prediction/"
-prediction.folder2 <- "8 - Prediction overlay/"
-
-
 create_prediction_plots <- function(path,width,height,difference.lag){ 
-
+# function that produces labelled overlays based on the classification and the original tracks:
+# different species are coloured
+# numbered objects without halo where filtered out before classification and are therefore artefacts,
+# though sometimes valid trajects are filtered out
+  
 trajectory.data <- predict_visual
 file_names <- unique(trajectory.data$file)  
 # change path for output
-dir.create(sub("2 - trajectory data/",prediction.folder,path))
+dir.create(paste0(path,prediction.folder))
 for (i in seq(1:length(file_names))){
-dir.create(paste0(path, prediction.folder, file_names[i])) 
+dir.create(paste0(path,prediction.folder,file_names[i],"/"))
 trajectory.data_tmp <- subset(trajectory.data,file == file_names[i])
   j<- 0
     while(j < max(trajectory.data$frame)+1){
-      jpeg(paste(sub(trajectory.data.folder,prediction.folder,path),as.character(file_names[i]),"/","frame_",j,".jpg",sep=""), width = as.numeric(width), height = as.numeric(height), quality = 100)
+      jpeg(paste(path,prediction.folder,as.character(file_names[i]),"/","frame_",j,".jpg",sep=""), width = as.numeric(width), height = as.numeric(height), quality = 100)
       par(mar = rep(0, 4), xaxs=c("i"), yaxs=c("i"))
       print <- subset(trajectory.data_tmp,trajectory.data_tmp$frame == j, select=c("X","Y","trajectory","predict_spec"))
       plot(print$Y, print$X+as.numeric(height), xlim=c(0,as.numeric(width)), ylim=c(0,as.numeric(height)), col=print$predict_spec, pch=1, cex=6, asp=1)
@@ -213,9 +139,9 @@ text <- readLines(paste(to.code, "ImageJ macros/Prediction_overlay.ijm", sep="")
   
   
 # use regular expression to insert input and output directory
-text[grep("avi_input = ", text)] <- paste("avi_input = ","'", sub(trajectory.data.folder,raw.video.folder,path),"';", sep = "")
-text[grep("overlay_input = ", text)] <- paste("overlay_input = ","'", sub(trajectory.data.folder,prediction.folder,path),"';", sep = "")
-text[grep("overlay_output = ", text)] <- paste("overlay_output = ","'", sub(trajectory.data.folder,prediction.folder2,path),"';", sep = "")
+text[grep("avi_input = ", text)] <- paste("avi_input = ","'", paste0(path,raw.video.folder),"';", sep = "")
+text[grep("overlay_input = ", text)] <- paste("overlay_input = ","'", paste0(path,prediction.folder),"';", sep = "")
+text[grep("overlay_output = ", text)] <- paste("overlay_output = ","'", paste0(path,prediction.folder2),"';", sep = "")
 text[grep("lag =", text)] <- paste("lag = ",difference.lag,";", sep = "")
   
 
@@ -242,10 +168,10 @@ system(cmd)
   
 # delete temporary file after execution
 if(.Platform$OS.type == "windows")
-   file.remove("C:/Program Files/Fiji.app/macros/Video_overlay_tmp.ijm")
+   file.remove("C:/Program Files/Fiji.app/macros/Prediction_overlay_tmp.ijm")
 }
 
-create_prediction_plots(path,width,height,difference.lag)
+create_prediction_plots(to.data,735,690,10)
 
 
 
