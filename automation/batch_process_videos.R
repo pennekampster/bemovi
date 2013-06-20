208e5c72fb1f61a38521994851e81266329d3a41
owen-petchey <owen.petchey@ieu.uzh.ch>
2013-06-20 13:54:07 +0200

    Cosmetic changes (mostly indentation)

--- automation/batch_process_videos.R
+++ automation/batch_process_videos.R
@@ -3,59 +3,60 @@
 video_to_trajectory <- function(video.dir, difference.lag, thresholds=c(10,255), stackmax.background="false")
 {
 
-## generate the folders...
-ijmacs.folder <- sub(raw.video.folder,"ijmacs/",video.dir)
-dir.create(ijmacs.folder, showWarnings = FALSE)	
-tmp.raw.folder <- sub(substr(raw.video.folder, 1, nchar(raw.video.folder)-1),"1 - raw tmp",video.dir)
-dir.create(tmp.raw.folder, showWarnings = FALSE)
-trajdata.folder <- sub(raw.video.folder,"2 - trajectory data/",video.dir)
-dir.create(trajdata.folder, showWarnings = FALSE)
-
-
-# copy master copy of ImageJ macro there for treatment
-text <- readLines(paste(to.code, "ImageJ macros/Video_to_trajectory.ijm", sep=""))
-
-# use regular expression to insert input and output directory
-text[grep("dir_input =", text)] <- paste("dir_input = ","'", video.dir,"';", sep = "")
-text[grep("dir_output =", text)] <- paste("dir_output = ","'",sub(raw.video.folder, paste(substr(raw.video.folder, 1, nchar(raw.video.folder)-1),  "tmp/"),video.dir),"';", sep = "")
-text[grep("lag =", text)] <- paste("lag = ",difference.lag,";", sep = "")
-text[grep("setThreshold", text)] <- paste("setThreshold(", thresholds[1], ",", thresholds[2], ");", sep="")
-if(stack.max.background=="light")
+    ## generate the folders...
+    ijmacs.folder <- sub(raw.video.folder,"ijmacs/",video.dir)
+    dir.create(ijmacs.folder, showWarnings = FALSE)	
+    tmp.raw.folder <- sub(substr(raw.video.folder, 1, nchar(raw.video.folder)-1),"1 - raw tmp",video.dir)
+    dir.create(tmp.raw.folder, showWarnings = FALSE)
+    trajdata.folder <- sub(raw.video.folder,"2 - trajectory data/",video.dir)
+    dir.create(trajdata.folder, showWarnings = FALSE)
+    
+    
+    ## copy master copy of ImageJ macro there for treatment
+    text <- readLines(paste(to.code, "ImageJ macros/Video_to_trajectory.ijm", sep=""))
+    
+    ## use regular expression to insert input and output directory
+    text[grep("dir_input =", text)] <- paste("dir_input = ","'", video.dir,"';", sep = "")
+    text[grep("dir_output =", text)] <- paste("dir_output = ","'",sub(raw.video.folder, paste(substr(raw.video.folder, 1, nchar(raw.video.folder)-1),  "tmp/"),video.dir),"';", sep = "")
+    text[grep("lag =", text)] <- paste("lag = ",difference.lag,";", sep = "")
+    text[grep("setThreshold", text)] <- paste("setThreshold(", thresholds[1], ",", thresholds[2], ");", sep="")
+    if(stack.max.background=="light")
 	text[grep("light =", text)] = paste("  light = ", stackmax.background, ";", sep="")
-
-
-
-# re-create ImageJ macro for batch processing of video files with ParticleTracker
-## perhaps put this in a subdirectory of the data folder?
-## This is implemented in OSX but not windows, which is as you wrote it
-if(.Platform$OS.type == "windows") 
+    
+    
+    
+    ## re-create ImageJ macro for batch processing of video files with ParticleTracker
+    ## perhaps put this in a subdirectory of the data folder?
+    ## This is implemented in OSX but not windows, which is as you wrote it
+    if(.Platform$OS.type == "windows") 
 	writeLines(text,con=paste("C:/Program Files/Fiji.app/macros/Video_to_trajectory_tmp.ijm",sep=""),sep="\n")
-if(.Platform$OS.type == "unix") 
-    writeLines(text,con=paste(ijmacs.folder, "/Video_to_trajectory_tmp.ijm",sep=""))
-
-
-# run to process video files by calling ImageJ / needs fixing for Mac
-if(.Platform$OS.type == "unix")
-    cmd <- paste("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ", paste(ijmacs.folder, "Video_to_trajectory_tmp.ijm",sep=""))
-if(.Platform$OS.type == "windows")
-    cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Video_to_trajectory_tmp.ijm')
-system(cmd)
-
-# delete temporary file after execution
-if(.Platform$OS.type == "windows")
-    file.remove("C:/Program Files/Fiji.app/macros/Video_to_trajectory_tmp.ijm")
-
-#copy files produced by ParticleTracker to "2 - trajectory data" directory
-all.files <- dir(tmp.raw.folder)
-ijout.files <- all.files[grep("Traj_", all.files)]
-file.copy(paste(sub("1 - raw/","1 - raw tmp/",video.dir),ijout.files, sep = ""),sub("1 - raw","2 - trajectory data",video.dir))
+    if(.Platform$OS.type == "unix") 
+        writeLines(text,con=paste(ijmacs.folder, "/Video_to_trajectory_tmp.ijm",sep=""))
+    
+    
+    ## run to process video files by calling ImageJ / needs fixing for Mac
+    if(.Platform$OS.type == "unix")
+        cmd <- paste("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ", paste(ijmacs.folder, "Video_to_trajectory_tmp.ijm",sep=""))
+    if(.Platform$OS.type == "windows")
+        cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Video_to_trajectory_tmp.ijm')
+    system(cmd)
+    
+    ## delete temporary file after execution
+    if(.Platform$OS.type == "windows")
+        file.remove("C:/Program Files/Fiji.app/macros/Video_to_trajectory_tmp.ijm")
+    
+    ## copy files produced by ParticleTracker to "2 - trajectory data" directory
+    all.files <- dir(tmp.raw.folder)
+    ijout.files <- all.files[grep("Traj_", all.files)]
+    file.copy(paste(sub("1 - raw/","1 - raw tmp/",video.dir),ijout.files, sep = ""),sub("1 - raw","2 - trajectory data",video.dir))
+    
 }
 
 
 
 
 ## This function gets the output files produced by the Imagej ParticleTracker
-# specify the path where the files which contain the trajectory data are stored
+## specify the path where the files which contain the trajectory data are stored
 LoadIJ_Traj_Outs <- function(trajdata.dir)
 {
   
@@ -67,20 +68,20 @@ for (i in 1:length(ijout.files)){
 text <- readLines(paste(trajdata.dir, ijout.files[i], sep="/"))
   
 string <- "(^[[:digit:]]+)"
-#find lines which match the string
+## find lines which match the string
 txtsubset <-  grep(string, text) 
 #subset the original file by only retaining lines that matched the string
 out <- text[txtsubset]
 
-# find lines that mark start of trajectories
+## find lines that mark start of trajectories
 pattern <- c("%% Trajectory [0_9]*")
 list <- grep(pattern,text)
 
-# add the end line of the last block and increase the line number by one to match the pattern of the other records
+## add the end line of the last block and increase the line number by one to match the pattern of the other records
 list_tmp <- append(list,length(text))
 list_tmp[length(list)+1]<-as.integer(list_tmp[length(list)+1]+1)
 
-# produce vector with unique trajectory ids
+## produce vector with unique trajectory ids
 difference <- numeric(0)
 for (k in 1:length(list))
 {diff <- rep(k,(list_tmp[k+1]-3-list_tmp[k]+1))
@@ -93,23 +94,23 @@ assign(paste(myfun(ijout.files[i])),ijout.files[i],envir = .GlobalEnv)
 if (i == 1){
 dd <- as.data.frame(read.table(textConnection(out)))
 dd$file <- rep(gsub(".avi.txt","",gsub("Traj_","",ijout.files[i])), length(dd$V1))
-# merge unique trajectory_ID with the original data
+## merge unique trajectory_ID with the original data
 dd <- cbind(dd,difference)
 }
 
 if (i > 1){
 dd.t <- as.data.frame(read.table(textConnection(out)))
 dd.t$file <- rep(gsub(".avi.txt","",gsub("Traj_","",ijout.files[i])), length(dd.t$V1))
-# merge unique trajectory_ID with the original data
+## merge unique trajectory_ID with the original data
 dd.t <- cbind(dd.t,difference)
 
 dd <- rbind(dd, dd.t)}
 }
 
-# invert Y-axis to account for origin in upper-left corner
+## invert Y-axis to account for origin in upper-left corner
 dd$V2 <- -dd$V2
 
-#rename drop unused information and rename variables
+## rename drop unused information and rename variables
 dd <- dd[c(-4,-5,-6,-7,-8,-9,-10)]
 names(dd) <- c("frame","X","Y","file","trajectory")
 
@@ -117,21 +118,21 @@ assign("trajectory.data",dd,envir = .GlobalEnv)
 write.table(trajectory.data, file = paste(trajdata.dir,"trajectory.data.txt", sep = "/"), sep = "\t")
 }
 
-# function to plot trajectories for overlay (must be merged with original video by ImageJ macro)
-# creates a folder containing one jpeg plot containing all the positions till the respective frame
-# for the moment colour not assigned by species identity but that's easy to add 
-# provide path of " 2 - trajectory data", and the width and height of the original video 
-# (I use cropped videos to increase speed while troubleshooting)
+## function to plot trajectories for overlay (must be merged with original video by ImageJ macro)
+## creates a folder containing one jpeg plot containing all the positions till the respective frame
+## for the moment colour not assigned by species identity but that's easy to add 
+## provide path of " 2 - trajectory data", and the width and height of the original video 
+## (I use cropped videos to increase speed while troubleshooting)
 create_overlay_plots <- function(path,width,height,difference.lag,type='traj'){ 
 trajectory.data <- as.data.frame(read.table(paste(path,"trajectory.data.txt", sep = ""), header = TRUE, sep = "\t"))
 file_names <- unique(trajectory.data$file)  
 
-# change path for output
+## change path for output
 dir.create(sub(trajectory.data.folder,overlay.folder,path))
 for (i in 1:length(file_names)){
-   #split filename into name and ending for creating directories according to video name
-   #filename_split <- strsplit(paste(file_names[i]),"\\.")
-   #filename <- filename_split[[1]]
+   ##split filename into name and ending for creating directories according to video name
+   ##filename_split <- strsplit(paste(file_names[i]),"\\.")
+   ##filename <- filename_split[[1]]
    dir.create(paste0(sub(trajectory.data.folder,overlay.folder,path), file_names[i])) #sub("Traj_","",filename[1]),sep="/"))
    trajectory.data_tmp <- subset(trajectory.data,file == file_names[i])
    j<- 0
@@ -155,7 +156,7 @@ for (i in 1:length(file_names)){
 
 }
 
-# copy master copy of ImageJ macro there for treatment
+## copy master copy of ImageJ macro there for treatment
 if(.Platform$OS.type == "windows")
   text <- readLines("C:/Users/Frank/Documents/PhD/Programming/franco/automation/ImageJ macros/Video_overlay.ijm",warn = FALSE)
 if(.Platform$OS.type == "unix")
@@ -164,14 +165,14 @@ if(.Platform$OS.type == "unix")
 text <- readLines(paste(to.code, "ImageJ macros/Video_overlay.ijm", sep=""))
 
 
-# use regular expression to insert input and output directory
+## use regular expression to insert input and output directory
 text[grep("avi_input = ", text)] <- paste("avi_input = ","'", sub(trajectory.data.folder,raw.video.folder,path),"';", sep = "")
 text[grep("overlay_input = ", text)] <- paste("overlay_input = ","'", sub(trajectory.data.folder,overlay.folder,path),"';", sep = "")
 text[grep("overlay_output = ", text)] <- paste("overlay_output = ","'", sub(trajectory.data.folder,overlay.folder2,path),"';", sep = "")
 text[grep("lag =", text)] <- paste("lag = ",difference.lag,";", sep = "")
 
 
-# re-create ImageJ macro for batch processing of video files with ParticleTracker
+## re-create ImageJ macro for batch processing of video files with ParticleTracker
 if(.Platform$OS.type == "windows")
   writeLines(text,con=paste("C:/Program Files/Fiji.app/macros/Video_overlay_tmp.ijm",sep=""),sep="\n")
 if(.Platform$OS.type == "unix") {
@@ -180,20 +181,20 @@ if(.Platform$OS.type == "unix") {
   ##writeLines(text,con=paste(sub("1 - raw","ijmacs",video.dir), "/Video_overlay_tmp.ijm",sep=""))
 }
   
-# create directory to store overlays
+## create directory to store overlays
 dir.create(sub(trajectory.data.folder,overlay.folder2,path))
   
-#call IJ macro to merge original video with the trajectory data
+## call IJ macro to merge original video with the trajectory data
 if(.Platform$OS.type == "unix"){
   cmd <- paste("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ", paste(sub(raw.video.folder,"ijmacs",video.dir), "/Video_overlay_tmp.ijm",sep=""))
   }
 if(.Platform$OS.type == "windows"){
 	cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Video_overlay_tmp.ijm')}
 
-# run ImageJ macro
+## run ImageJ macro
 system(cmd)
 
-# delete temporary file after execution
+## delete temporary file after execution
 if(.Platform$OS.type == "windows")
   file.remove("C:/Program Files/Fiji.app/macros/Video_overlay_tmp.ijm")
 }
@@ -260,14 +261,14 @@ Check_threshold <- function(video.dir,difference.lag, thresholds=c(10,255)) {
 
 
 create_prediction_plots <- function(path,width,height,difference.lag){ 
-  # function that produces labelled overlays based on the classification and the original tracks:
-  # different species are coloured
-  # numbered objects without halo where filtered out before classification and are therefore artefacts,
-  # though sometimes valid trajects are filtered out
+  ## function that produces labelled overlays based on the classification and the original tracks:
+  ## different species are coloured
+  ## numbered objects without halo where filtered out before classification and are therefore artefacts,
+  ## though sometimes valid trajects are filtered out
   
   trajectory.data <- predict_visual
   file_names <- unique(trajectory.data$file)  
-  # change path for output
+  ## change path for output
   dir.create(paste0(path,prediction.folder))
   for (i in seq(1:length(file_names))){
     dir.create(paste0(path,prediction.folder,file_names[i],"/"))
@@ -282,7 +283,7 @@ create_prediction_plots <- function(path,width,height,difference.lag){
       dev.off()
       j <- j+1}}
   
-  # copy master copy of ImageJ macro there for treatment
+  ## copy master copy of ImageJ macro there for treatment
   if(.Platform$OS.type == "windows")
     text <- readLines("C:/Users/Frank/Documents/PhD/Programming/franco/automation/ImageJ macros/Video_overlay.ijm",warn = FALSE)
   if(.Platform$OS.type == "unix")
@@ -291,14 +292,14 @@ create_prediction_plots <- function(path,width,height,difference.lag){
   text <- readLines(paste(to.code, "ImageJ macros/Prediction_overlay.ijm", sep=""))
   
   
-  # use regular expression to insert input and output directory
+  ## use regular expression to insert input and output directory
   text[grep("avi_input = ", text)] <- paste("avi_input = ","'", paste0(path,raw.video.folder),"';", sep = "")
   text[grep("overlay_input = ", text)] <- paste("overlay_input = ","'", paste0(path,prediction.folder),"';", sep = "")
   text[grep("overlay_output = ", text)] <- paste("overlay_output = ","'", paste0(path,prediction.folder2),"';", sep = "")
   text[grep("lag =", text)] <- paste("lag = ",difference.lag,";", sep = "")
   
   
-  # re-create ImageJ macro for batch processing of video files with ParticleTracker
+  ## re-create ImageJ macro for batch processing of video files with ParticleTracker
   if(.Platform$OS.type == "windows")
     writeLines(text,con=paste("C:/Program Files/Fiji.app/macros/Prediction_overlay_tmp.ijm",sep=""),sep="\n")
   if(.Platform$OS.type == "unix") {
@@ -306,20 +307,20 @@ create_prediction_plots <- function(path,width,height,difference.lag){
     writeLines(text,con=paste(ijmacs.folder, "/Prediction_overlay_tmp.ijm",sep=""))
   }
   
-  # create directory to store overlays
+  ## create directory to store overlays
   dir.create(paste0(path,prediction.folder2))
   
-  #call IJ macro to merge original video with the trajectory data
+  ## call IJ macro to merge original video with the trajectory data
   if(.Platform$OS.type == "unix"){
     cmd <- paste("java -Xmx8192m -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -macro ", paste(sub(raw.video.folder,"ijmacs",video.dir), "/Prediction_overlay_tmp.ijm",sep=""))
   }
   if(.Platform$OS.type == "windows"){
     cmd <- c('"C:/Program Files/FIJI.app/fiji-win64.exe" -macro Prediction_overlay_tmp.ijm')}
   
-  # run ImageJ macro
+  ## run ImageJ macro
   system(cmd)
   
-  # delete temporary file after execution
+  ## delete temporary file after execution
   if(.Platform$OS.type == "windows")
     file.remove("C:/Program Files/Fiji.app/macros/Prediction_overlay_tmp.ijm")
 }
