b6d440569baffbc0ff12ff4b9c2eff39cbe203f6
Frank Pennekamp <pennekampster@gmail.com>
2014-07-22 17:58:39 +0200

    Function fully documented

--- R/check_video_file_names.R
+++ R/check_video_file_names.R
@@ -1,12 +1,13 @@
-#' Function to check if there are any unsupported file types or names
+#' Function to check that video files have extension cxd or avi, and that they are otherwise compatible.
 #' 
 #' Checks the files in the raw data for the supported avi and cxd file formats and that file names do not 
 #' contain periods except before the file type extension
-#' @param path Path to the raw videos to be treated
-#' @return Returns an error message and a list with unsupported files or names
+#' @param to.data path to the working directory 
+#' @param raw.video.folder directory with the raw video files
+#' @return returns an error message and a list with unsupported files or names
 #' @export
 
-Check_video_file_names <- function(to.data, raw.video.folder) {
+check_video_file_names <- function(to.data, raw.video.folder) {
   video.dir <- paste(to.data, raw.video.folder, sep = "")
   files <- dir(video.dir)
   
@@ -14,6 +15,7 @@ Check_video_file_names <- function(to.data, raw.video.folder) {
   unsupported.files <- files[-c(grep("\\.avi", files), grep("\\.cxd", files))]
   if (length(unsupported.files) > 0) 
     print(paste("Unsupported video file:", unsupported.files))
+ 
   ## check for files with more than one period; I think this previously caused me a problem
   bad.filenames <- files[unlist(lapply(lapply(strsplit(files, "\\."), length), function(x) x > 2))]
   if (length(bad.filenames) > 0) 
