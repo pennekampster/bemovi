583de9be73d2a662148bf047a905a62da5a9ca41
Frank Pennekamp <pennekampster@gmail.com>
2014-07-28 15:24:48 +0200

    Calculate movement function integrated into the link_particles function to simplify the package. Calculate_mvt is no longer exported as a user function and the functions used for calculating movement metrics were transferred into a separate function script.

--- R/Calculate_mvt.R
+++ R/Calculate_mvt.R
@@ -4,99 +4,12 @@
 #' such as the gross and net displacement, absolute and relative angles and duration
 #' 
 #' @param data dataframe containing the X- and Y-coordinates, the frame and the trajectory ID
-#' @param write Saves the original dataframe with movement metrics attached to the disk
 #' @param to.data path to the working directory 
 #' @param merged.data.folder directory where the data is saved, if write is TRUE
 #' @return returns a data.table with the movement metrics for each fix appended to the original data (NB: movement metrics often need two (e.g. step length), sometimes even 
 #' three fixes (turning angles) to be calculated; fixes for which metrics cannot be calculated are padded with NA)
-#' @export
 
-calculate_mvt <- function(data,write=FALSE,to.data,merged.data.folder){
-
-# define the functions to calculate movement parameters
-anglefun <- function(xx,yy,bearing=TRUE,as.deg=FALSE){
-  
-  ## http://quantitative-ecology.blogspot.ch/2007/05/anglefun-function-xxyy-bearing-true-as.html
-  ## function changed such that angles are reported with the x-axis as North (see comment thread)
-  ## calculates the compass bearing of the line between two points
-  ## xx and yy are the differences in x and y coordinates between two points
-  ## Options:
-  ## bearing = FALSE returns +/- pi instead of 0:2*pi
-  ## as.deg = TRUE returns degrees instead of radians
-  c = 1
-  if (as.deg){
-    c = 180/pi
-  }
-  
-  b<-sign(yy)
-  b[b==0]<-1  #corrects for the fact that sign(0) == 0
-  tempangle = b*(xx<0)*pi+atan(yy/xx)
-  
-  
-  if(bearing){
-    #return a compass bearing 0 to 2pi
-    #if bearing==FALSE then a heading (+/- pi) is returned
-    tempangle[tempangle<0]<-tempangle[tempangle<0]+2*pi
-  }
-  tempangle <- tempangle*c
-  tempangle <- c(tempangle,-9999)
-  tempangle[tempangle==-9999] <- NA
-  return(tempangle)
-}
-
-# shortened function to calculate turning angles from absolute angles
-rel.angle <- function(abs.angle){
- 
-  abs.angle <- na.omit(abs.angle)
-  
-  if (length(abs.angle)>2){
-  obs <- length(abs.angle)
-  ta <- abs.angle[2:(obs)]-abs.angle[1:(obs-1)]
-  ta[ta < -pi] = ta[ta < -pi] + 2*pi
-  ta[ta > pi] = ta[ta > pi] - 2*pi
-  ta <- c(-9999,ta,-9999)
-  ta[ta == -9999] <- NA
-  ta <- as.numeric(ta)
-  return(ta)}
-  
-  else {
-  obs <- (length(abs.angle)+1)
-  ta <- rep(NA,obs)
-  ta <- as.numeric(ta)
-  return(ta)}
-}
-
-step_length <- function(x,y){
-
-# function to extract step lengths from X- & Y-coordinates
-  
-  step <- sqrt(diff(x)^2+diff(y)^2)  
-  step <- c(step,-9999)
-  step[step == -9999] <- NA
-  return(step)  
-  
-}
-
-step_duration <- function(frame_){
-  
-  # function to extract step duration for each step
-  step_duration <- diff(frame_)
-  step_duration <- c(step_duration,-9999)
-  step_duration[step_duration == -9999] <- NA
-  return(step_duration)  
-  
-}
-
-
-net_displacement <- function(x,y){
-
-# function to calculate the net squared displacement for each step
-   start_x <- x[1]
-   start_y <- y[1]
-   R2n <- (start_x-x)^2+(start_y-y)^2
-   return(R2n)
-
-}
+calculate_mvt <- function(data,to.data,merged.data.folder){
 
   # output path
   out.dir <- paste0(to.data,merged.data.folder)
@@ -122,7 +35,7 @@ net_displacement <- function(x,y){
   #rename frame column to avoid clashes with frame() function
   setnames(data, c("file","X","Y","frame","id","trajectory"), c("file","X","Y","frame_","id","trajectory"))
 
-mvt_summary <- data[,list(frame = frame_,
+  mvt_summary <- data[,list(frame = frame_,
                          step_length = step_length(X,Y),
                          step_duration = step_duration(frame_),
                          step_speed = step_length(X,Y)/step_duration(frame_),
@@ -131,13 +44,12 @@ mvt_summary <- data[,list(frame = frame_,
                          abs_angle = anglefun(diff(X),diff(Y)),
                          rel_angle = rel.angle(anglefun(diff(X),diff(Y)))), by=id]
 
-mvt_summary <- mvt_summary[ , list(id,frame,step_length, step_duration, step_speed, gross_disp, net_disp, abs_angle, rel_angle)]
+  mvt_summary <- mvt_summary[ , list(id,frame,step_length, step_duration, step_speed, gross_disp, net_disp, abs_angle, rel_angle)]
 
-trajectory.data <- merge(data_full,mvt_summary,by=c("id","frame"), all.x=T)
+  trajectory.data <- merge(data_full,mvt_summary,by=c("id","frame"), all.x=T)
 
-if (write==TRUE){save(trajectory.data, file = paste0(out.dir,"Master.Rdata"))}
-
-return(trajectory.data)
+  save(trajectory.data, file = paste0(out.dir,"trajectory.RData"))
+  #return(trajectory.data)
 }
 
 
