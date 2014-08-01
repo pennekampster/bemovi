549ab1dcedb4fba4be384d3b59cb8163631bda58
Frank Pennekamp <Frank.Pennekamp@uclouvain.be>
2014-08-01 20:05:26 +0200

    Corrected bug with ParticleLinker under Windows (now also allowing spaces in directory names)

--- R/Link_particles.R
+++ R/Link_particles.R
@@ -51,15 +51,16 @@ link_particles <- function(to.data, particle.data.folder, trajectory.data.folder
       }
       
       if (.Platform$OS.type == "windows") {
-        cmd <- paste0("C:/Progra~2/java/jre7/bin/javaw.exe -Xmx", memory, "m -Dparticle.linkrange=", linkrange, 
-                      " -Dparticle.displacement=", disp, " -jar ", to.particlelinker, "/ParticleLinker.jar ", "'", dir, "'", " \"", 
-                      traj_out.dir, "/ParticleLinker_", all.files[j], "\"")
         
-        print(cmd)
+       cmd <- paste0("C:/Progra~2/java/jre7/bin/javaw.exe -Xmx512m -Dparticle.linkrange=5 -Dparticle.displacement=20 -jar",
+                      gsub("/","\\\\", paste0(" " ,to.particlelinker,"/ParticleLinker.jar")),
+                      gsub("/","\\\\", paste0(" ","\"" ,dir,"\"")),
+                      gsub("/","\\\\", paste0(" ","\"", traj_out.dir, "/ParticleLinker_", all.files[j], "\"")))
+          
         system(cmd)
       }
       
-      # delete working dir
+      #delete working dir
       unlink(dir, recursive = TRUE)
                 
     }
@@ -77,3 +78,28 @@ link_particles <- function(to.data, particle.data.folder, trajectory.data.folder
   #calculate movement metrics for each fix and save to disk
   calculate_mvt(data,to.data,trajectory.data.folder)
 }
+
+
+# 
+# linkrange <- 5
+# memory <- 512
+# disp <- 20
+# 
+# to.particlelinker <- "C:/Users/Frank/Documents/PhD/Programming/Franco_data"
+# 
+# dir1 <- paste(gsub('/',"\\\\","C:\Users\Frank\Documents\PhD\Programming\Franco_data\data\Data38")
+# traj_out.dir <- ""
+# 
+# all.files <- c("1","2")
+# j <- 1
+# 
+# cmd <- paste0("C:/Progra~2/java/jre7/bin/javaw.exe -Xmx", memory, "m -Dparticle.linkrange=", linkrange, 
+#               " -Dparticle.displacement=", disp, " -jar ", to.particlelinker, "/ParticleLinker.jar ", 
+#               "'", "C:/Users/Frank/Documents/PhD/Programming/Franco_data/data/Data38", traj_out.dir, "/ParticleLinker_", all.files[j], "'")
+# 
+# 
+# gsub('\\', "\\\\", traj_out.dir)
+# 
+# 
+# 
+# 
