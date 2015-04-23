2b216c530f5de8fd423bf8531df7bd8ea0a4d6e3
efronhofer <emanuel.fronhofer@eawag.ch>
2015-04-23 14:45:37 +0200

    population level calculations added fro some selected morphological and movement parameters; also densities

--- R/Summarize_PopulationLevel.R
+++ R/Summarize_PopulationLevel.R
@@ -11,15 +11,42 @@
 #' @return returns a data.table with the aggregated morphology and movement information for each trajectory
 #' @export
 
-summarize_populations <- function(data, write=FALSE, to.data, merged.data.folder){
+summarize_populations <- function(traj.data, sum.data, write=FALSE, to.data, merged.data.folder, video.description.folder, video.description.file){
   
   # checks whether frames per second are specified
   if(!exists("fps") ) stop("frames per second not specified (fps)")
   # checks whether the sample volume is specified
   if(!exists("measured_volume") ) stop("measured volume not specified (measured_volume)")
   
-  # results object
-  #pop_output <- 
+  # results object from video description file
+  pop_output <- read.table(paste(to.data, video.description.folder, video.description.file, sep = ""), sep = "\t", header = TRUE)
+  
+  # now add the population densities
+  pop_count_table <- tapply(traj.data$Major,list(as.factor(traj.data$file),as.factor(traj.data$frame)),length)
+  pop_output$counts <- as.numeric(rowMeans(pop_count_table))
+  pop_output$indiv_per_vol <- as.numeric(rowMeans(pop_count_table))/measured_volume
+  
+  # first get file from id
+  sum.data$file <- sub("-.*$", "", sum.data$id )
+  
+  # get morphology
+  pop_output$major_mean <- as.numeric(tapply(sum.data$major,sum.data$file,mean,na.rm=T))
+  pop_output$major_sd <- as.numeric(tapply(sum.data$major,sum.data$file,sd,na.rm=T))
+  pop_output$minor_mean <- as.numeric(tapply(sum.data$minor,sum.data$file,mean,na.rm=T))
+  pop_output$minor_sd <- as.numeric(tapply(sum.data$minor,sum.data$file,sd,na.rm=T))
+  
+  # calculate gross speed
+  sum.data$gross_speed <- sum.data$gross_disp/sum.data$duration 
+  
+  # get movement
+  pop_output$gross_speed_mean <- as.numeric(tapply(sum.data$gross_speed,sum.data$file,mean,na.rm=T))
+  pop_output$gross_speed_sd <- as.numeric(tapply(sum.data$gross_speed,sum.data$file,sd,na.rm=T))
+  
+  pop_output$net_speed_mean <- as.numeric(tapply(sum.data$net_speed,sum.data$file,mean,na.rm=T))
+  pop_output$net_speed_sd <- as.numeric(tapply(sum.data$net_speed,sum.data$file,sd,na.rm=T))
+  
+  pop_output$sd_turning_mean <- as.numeric(tapply(sum.data$sd_turning,sum.data$file,mean,na.rm=T))
+  
   
   #output population summary data
   if (write==TRUE){save(pop_output, file = paste0(to.data, merged.data.folder,"Population_Data.RData"))}
