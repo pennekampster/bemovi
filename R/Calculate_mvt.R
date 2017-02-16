1bafe06979df5ffffd529b5921873586f7c3a658
Frank Pennekamp <pennekampster@gmail.com>
2017-02-16 15:05:14 +0100

    Fix against breaking data.table dependency

--- R/Calculate_mvt.R
+++ R/Calculate_mvt.R
@@ -41,7 +41,7 @@ calculate_mvt <- function(data,to.data,trajectory.data.folder,pixel_to_scale,fps
   data <- data[!is.na(data$trajectory),]
   
   #filter out duplicate positions, if available
-  data <- data[!(diff(X) == 0 & diff(Y) == 0),]
+  data <- data[!(diff(c(FALSE, X)) == 0 & diff(c(FALSE, Y)) == 0)]
   
   #subset dataset to only include relevant movement information
   data <- data[,list(file,X,Y,frame,id,trajectory)]
